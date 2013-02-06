214c3a493043260bfe8cefe383193b695c14677f
Calvin Fisher <calvin.fisher@gmail.com>
2013-02-06 10:36:47 -0500

    Overhauled ToastView
    Rearranged & cleaned up code, made more extensible, added static Show
    methods.

--- ToastView.cs
+++ ToastView.cs
@@ -4,7 +4,7 @@ using MonoTouch.UIKit;
 using System.Drawing;
 using MonoTouch.ObjCRuntime;
 
-// From http://stackoverflow.com/questions/4221169/android-toast-in-iphone
+// Inspired by http://stackoverflow.com/questions/4221169/android-toast-in-iphone
 
 namespace MonoTouchTools
 {
@@ -12,145 +12,153 @@ namespace MonoTouchTools
     /// Android-like Toast popup.
     /// </summary>
     /// <remarks>
-    /// Works like MonoTouch UIAlertView - just create and Show().
+    /// Create and Show() like MonoTouch UIAlertView, or static Show() like .NET MessageBox.
     /// </remarks>        
     public class ToastView : NSObject
     {
         public string Text { get; protected set; }
+        public int DurationMilliseconds { get; set; }
+        public double DurationSeconds { get { return DurationMilliseconds / 1000; } }
         public UITextAlignment TextAlignment { get; set; }
+        public ToastGravity Gravity { get; set; }
+        public int OffsetLeft { get; set; }
+        public int OffsetTop { get; set; }
+        public int Padding { get; set; }
+        public UIFont Font { get; set; }
+        public UIColor TextColor { get; set; }
+        public UIColor BackgroundColor { get; set; }
+        public UIColor ShadowColor { get; set; }
+        public int ShadowOffset { get; set; }
 
-        protected ToastSettings theSettings = new ToastSettings ();
+        protected const int defaultDurationMilliseconds = 1000;
+        protected const UITextAlignment defaultTextAlignment = UITextAlignment.Center;
+        protected const int defaultPadding = 15;
+        protected const ToastGravity defaultGravity = ToastGravity.Center;
+        protected const int defaultOffsetLeft = 0;
+        protected const int defaultOffsetTop = 0;
+
+        protected UIButton button;
         protected UIView view;
-        protected int offsetLeft = 0;
-        protected int offsetTop = 0;
+        protected UIWindow window;
 
-        public ToastView (string text)
+        public ToastView (string text): this (text, defaultDurationMilliseconds) {}
+        public ToastView (string text, int durationMilliseconds) : this (text, durationMilliseconds, defaultGravity) {}
+        public ToastView (string text, int durationMilliseconds, ToastGravity gravity) : this (text, durationMilliseconds, gravity, defaultPadding) {}
+        public ToastView (string text, int durationMilliseconds, ToastGravity gravity, int padding) : this (text, durationMilliseconds, gravity, padding, defaultTextAlignment) {}
+        public ToastView (string text, int durationMilliseconds, ToastGravity gravity, int padding, UITextAlignment textAlignment)
         {
             Text = text;
-            TextAlignment = UITextAlignment.Center; 
+            DurationMilliseconds = durationMilliseconds;
+            TextAlignment = textAlignment;
+            Padding = padding;
+            Gravity = gravity;
+
+            button = UIButton.FromType (UIButtonType.Custom);
+            view = button;
+            window = UIApplication.SharedApplication.Windows[0];
+
+            OffsetLeft = 0;
+            OffsetTop = 0;
+            Font = UIFont.SystemFontOfSize (16);
+            TextColor = UIColor.White;
+            BackgroundColor = UIColor.FromRGBA (0, 0, 0, 0.7f);
+            ShadowColor = UIColor.DarkGray;
+            ShadowOffset = 1;
         }
-        public ToastView (string text, int durationMilliseonds)
-            : this(text)
+
+        public static void Show (string text)
         {
-            theSettings.Duration = durationMilliseonds;
+            new ToastView(text).Show();
         }
-        public ToastView SetGravity (ToastGravity gravity, int OffSetLeft, int OffSetTop)
+        public static void Show (string text, int durationMilliseconds)
         {
-            theSettings.Gravity = gravity;
-            offsetLeft = OffSetLeft;
-            offsetTop = OffSetTop;
-            return this;
+            new ToastView(text, durationMilliseconds).Show();
         }
-        
-        public ToastView SetPosition (PointF Position)
+        public static void Show (string text, int durationMilliseconds, ToastGravity gravity)
         {
-            theSettings.Position = Position;
-            return this;
+            new ToastView(text, durationMilliseconds, gravity).Show();
         }
-        
+        public static void Show (string text, int durationMilliseconds, ToastGravity gravity, int padding)
+        {
+            new ToastView(text, durationMilliseconds, gravity, padding).Show();
+        }
+        public static void Show (string text, int durationMilliseconds, ToastGravity gravity, int padding, UITextAlignment textAlignment)
+        {
+            new ToastView (text, durationMilliseconds, gravity, padding, textAlignment).Show();
+        }
+
         public void Show ()
         {
-            UIButton v = UIButton.FromType (UIButtonType.Custom);
-            view = v;
+            var textSize = view.StringSize (Text, Font, new SizeF (280,60));
+            UILabel label = new UILabel (new RectangleF (0, 0, textSize.Width + 5, textSize.Height + 5))
+            {
+                TextColor = TextColor,
+                BackgroundColor = UIColor.Clear,
+                Font = Font,
+                Text = Text,
+                Lines = 0,
+                ShadowColor = ShadowColor,
+                ShadowOffset = new SizeF (ShadowOffset, ShadowOffset),
+                TextAlignment = TextAlignment,
+            };
+            
+            button.Frame = new RectangleF (0, 0, textSize.Width + Padding, textSize.Height + Padding);
+            label.Center = new PointF (button.Frame.Size.Width / 2, button.Frame.Height / 2);
 
-            UIFont font = UIFont.SystemFontOfSize (16);
-            SizeF textSize = view.StringSize (Text, font, new SizeF (280, 60));
+            button.BackgroundColor = BackgroundColor;
+            button.Layer.CornerRadius = 5;
+            button.Center = Position;
+
+            button.AddSubview (label);
+            window.AddSubview (button);
+            button.AllTouchEvents += delegate { HideToast (); };
             
-            UILabel label = new UILabel (new RectangleF (0, 0, textSize.Width + 5, textSize.Height + 5));
-            label.BackgroundColor = UIColor.Clear;
-            label.TextColor = UIColor.White;
-            label.Font = font;
-            label.Text = Text;
-            label.Lines = 0;
-            label.ShadowColor = UIColor.DarkGray;
-            label.ShadowOffset = new SizeF (1, 1);
-            label.TextAlignment  = TextAlignment;
-            
-            v.Frame = new RectangleF (0, 0, textSize.Width + 10, textSize.Height + 10);
-            label.Center = new PointF (v.Frame.Size.Width / 2, v.Frame.Height / 2);
-            v.AddSubview (label);
-            
-            v.BackgroundColor = UIColor.FromRGBA (0, 0, 0, 0.7f);
-            v.Layer.CornerRadius = 5;
-            
-            UIWindow window = UIApplication.SharedApplication.Windows[0];
-            
-            PointF point = new PointF (window.Frame.Size.Width / 2, window.Frame.Size.Height / 2);
-            
-            if (theSettings.Gravity == ToastGravity.Top)
-            {
-                point = new PointF (window.Frame.Size.Width / 2, 45);
-            }
-            else if (theSettings.Gravity == ToastGravity.Bottom)
-            {
-                point = new PointF (window.Frame.Size.Width / 2, window.Frame.Size.Height - 45);
-            }
-            else if (theSettings.Gravity == ToastGravity.Center)
-            {
-                point = new PointF (window.Frame.Size.Width / 2, window.Frame.Size.Height / 2);
-            }
-            else
-            {
-                point = theSettings.Position;
-            }
-            
-            point = new PointF (point.X + offsetLeft, point.Y + offsetTop);
-            v.Center = point;
-            window.AddSubview (v);
-            v.AllTouchEvents += delegate { HideToast (); };
-            
-            NSTimer.CreateScheduledTimer (theSettings.DurationSeconds, HideToast);
-            
+            NSTimer.CreateScheduledTimer (DurationSeconds, HideToast);
         }
-        
-        
-        void HideToast ()
+
+        public PointF Position
+        {
+            get
+            {
+                switch (Gravity)
+                {
+                    case ToastGravity.Top:
+                        return new PointF(
+                            (window.Frame.Size.Width / 2) + OffsetLeft, 
+                            45 + OffsetTop
+                            );
+                        
+                    case ToastGravity.Bottom:
+                        return new PointF(
+                            (window.Frame.Size.Width / 2) + OffsetLeft, 
+                            window.Frame.Size.Height - 45 + OffsetTop
+                            );
+                        
+                    case ToastGravity.Center:
+                        return new PointF (
+                            (window.Frame.Size.Width / 2) + OffsetLeft,
+                            (window.Frame.Size.Height / 2) + OffsetTop
+                            );
+                        
+                    default:
+                        throw new IndexOutOfRangeException("Unknown ToastGravity value " + Gravity);
+                }
+            }
+        }
+
+        protected void HideToast ()
         {
             UIView.BeginAnimations ("");
             view.Alpha = 0;
             UIView.CommitAnimations ();
         }
         
-        void RemoveToast ()
+        protected void RemoveToast ()
         {
             view.RemoveFromSuperview ();
         }
-        
     }
-
-    public class ToastSettings
-    {
-        public ToastSettings ()
-        {
-            this.Duration = 500;
-            this.Gravity = ToastGravity.Center;
-        }
-        
-        public int Duration
-        {
-            get;
-            set;
-        }
-        
-        public double DurationSeconds
-        {
-            get { return (double) Duration/1000 ;}
-            
-        }
-        
-        public ToastGravity Gravity
-        {
-            get;
-            set;
-        }
-        
-        public PointF Position
-        {
-            get;
-            set;
-        }
-    }
-
+    
     public enum ToastGravity
     {
         Top = 0,
